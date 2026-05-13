93305aa31e67fb18382fb20cecbc5fc90fa481ba
Toshiki <1114iiit0shiki@gmail.com>
2026-05-13 14:57:56 +0900

    clocktest

--- clock.py
+++ clock.py
@@ -1,4 +1,5 @@
 from citam_pydraw import *
+import math
 
 @animation(True)
 def draw():
@@ -10,7 +11,41 @@ def draw():
     Line(300, 150, 300, 150)
     Line(300, 300, 300, 300)
 
-    dial = Ellipse(150, 150, 290, 290)
+    # 水
+    t = date.second + date.milli_second / 1000
+    ratio = s / 60
+
+    cx = 150
+    cy = 150
+    r = 140
+
+    base_y = cy + r - (2 * r * ratio)
+
+    for x in range(10, 290, 2):
+        dx = x - cx
+
+        if dx*dx > r*r:
+            continue
+
+        # 円の上下
+        circle_top = cy - math.sqrt(r*r - dx*dx)
+        circle_bottom = cy + math.sqrt(r*r - dx*dx)
+
+        # 波
+        wave = math.sin(t * 2 + x * 0.04) * 3
+        y = base_y + wave
+
+        # 上も下も円の中に制限
+        if y < circle_top:
+            y = circle_top
+        if y > circle_bottom:
+            y = circle_bottom
+
+        water_line = Line(x, y, x, circle_bottom, 2)
+        water_line.fill(color(120, 200, 255))
+
+    #外枠
+    dial = Ellipse(150, 150, 280, 280)
     dial.noFill()
     dial.outlineFill(color(0, 0, 0))
 
@@ -32,22 +67,34 @@ def draw():
     byo.setRotationCenter(150, 150)
     byo.rotate(s*360/60)
 
-    text3 = Text("12", 150,15)
-    text4 = Text("1", 225,35)
-    text5 = Text("2", 265,80)
-    text6 = Text("3", 290,150)
-    text7 = Text("4", 265,210)
-    text8 = Text("5", 220,260)
-    text9 = Text("6", 150,280)
-    text10 = Text("7", 80,260)
-    text11 = Text("8", 40,210)
-    text12 = Text("9", 15,150)
-    text13 = Text("10", 30,80)
-    text14 = Text("11", 70,35)
+    text12 = Text("12", 150, 20)
+    text3 = Text("3", 280, 150)
+    text6 = Text("6", 150, 280)
+    text9 = Text("9", 20, 150)
+
+    # 目盛り（1〜11）
+    for i in range(1, 12):
+        if i == 3 or i == 6 or i == 9:
+            continue
+
+        tick = Line(150, 20, 150, 40, 2)
+        tick.setRotationCenter(150, 150)
+        tick.rotate(i * 360 / 12)
+
+    global pflag
+    s = date.second
+    #時報（1分毎に音を鳴らす）
+    if (int(s)%60 == 0) and pflag:#秒の値が0になった時という条件と最初の再生という条件
+        player.play()#音源ファイルを再生する
+        pflag = False #毎0秒で1回だけ再生する
+        #↑0秒だけど2回目以降はpflagがFalseになるので条件を満たさないから再生されない
+    elif (int(s)%60 != 0) and not pflag: 
+        pflag = True
 
 if __name__ == "__main__":
     window = Window(300, 300).title("Clock").background(color(250,250,250))
     date = Date()
+    player = loadMusic("water.mp3")
     pflag = True
     draw()
     window.show()
